bd837b5eb39059aa69cb77c8d7e0f39d040f55c3
BenRasha <bben.rasha@gmail.com>
2019-10-09 20:17:46 +0300

    Added Programs

---
+++ src/Epam/BasicsOfSoftwareCodeDevelopment/Linear/LinearProgramm6.java
@@ -0,0 +1,37 @@
+package Epam.BasicsOfSoftwareCodeDevelopment.Linear;
+
+import java.util.Scanner;
+public class LinearProgramm6 {
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Input your x and y:");
+        int x=scanner.nextInt();
+        int y=scanner.nextInt();
+        System.out.println("("+x+","+y+")");
+        //upper rectangle//
+        int MaxRightURectangleX=2;
+        int MaxLeftURectangleX=-2;
+        int MaxUpURectangleY=4;
+        int MaxDownURectangleY=0;
+        boolean UpperFlag=(x<=MaxRightURectangleX && x>=MaxLeftURectangleX) && (y<=MaxUpURectangleY && y>=MaxDownURectangleY);
+        //lower rectangle//
+        int MaxLeftLRectangleX=-4;
+        int MaxRightLRectangleX=4;
+        int MaxDownLRectangleY=-3;
+        int MaxUpLRectangleY=0;
+        boolean LowerFlag=(x<=MaxRightLRectangleX && x>=MaxLeftLRectangleX) && (y>=MaxDownLRectangleY && y<=MaxUpLRectangleY);
+        //circle//
+        double MaxRightCircle=0.5;
+        double MaxLeftCircle=-0.5;
+        double MaxUpCircle=-0.5;
+        double MaxDownCircle=-1.5;
+        boolean CircleFlag=(x<=MaxRightCircle && x>=MaxLeftCircle) && (y<=MaxUpCircle && y>=MaxDownCircle);
+        if(UpperFlag || LowerFlag != CircleFlag)
+        {
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
