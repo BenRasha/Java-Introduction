f091423d1d893ee1284c028a340789815c864d8d
BenRasha <bben.rasha@gmail.com>
2019-11-04 20:00:12 +0300

    Initial commit

---
+++ src/Epam/Algorithms/OneDimentional2.java
@@ -0,0 +1,31 @@
+package Epam.Algorithms;
+
+import java.util.Scanner;
+
+public class OneDimentional2 {
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Input array size:");
+        int n=scanner.nextInt();
+        double []arr=new double[n];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=Math.random()*100;
+            System.out.println("["+(i+1)+"]"+arr[i]);
+        }
+        System.out.print("Input checking number:");
+        double z=scanner.nextDouble();
+        int flag=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>z)
+            {
+                arr[i]=z;
+                flag++;
+            }
+            System.out.println("["+(i+1)+"]"+arr[i]);
+        }
+        System.out.print("Number of replacements:"+flag);
+    }
+}
---
+++ src/Epam/Algorithms/OneDimentional3.java
@@ -0,0 +1,38 @@
+package Epam.Algorithms;
+
+import java.util.Scanner;
+
+public class OneDimentional3 {
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Input size of array:");
+        int n=scanner.nextInt();
+        double []arr=new double[n];
+        int positiveFlag=0;
+        int negativeFlag=0;
+        int zeroFlag=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=-1+Math.random()*2;
+            System.out.println("["+(i+1)+"]"+arr[i]);
+            if(arr[i]>0)
+            {
+                positiveFlag++;
+            }else if(arr[i]<0)
+            {
+                negativeFlag++;
+            }else{
+                zeroFlag++;
+            }
+        }
+        System.out.print("Number of positive elements:"+positiveFlag+'\n');
+        System.out.print("Number of negative elements:"+negativeFlag+'\n');
+        if(zeroFlag>0)
+        {
+            System.out.print("Number of zeros:"+zeroFlag);
+        }else{
+            System.out.print("There are no zeros");
+        }
+    }
+}
