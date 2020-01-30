bbda6b0b0775e31ace8d313d0b40007081bf91cd
BenRasha <bben.rasha@gmail.com>
2020-01-30 20:15:59 +0300

    Initial commit

---
+++ src/Epam/Algorithms/ArraysOfArrays/Task4.java
@@ -0,0 +1,47 @@
+package Epam.Algorithms.ArraysOfArrays;
+
+import java.util.Scanner;
+public class Task4 {
+    public static void input(int arr[][], int n)
+    {
+        int count=1;
+        int flag=n;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i%2==0) {
+                    arr[i][j]=count;
+                    count++;
+                }
+                else{
+                    arr[i][j]=flag;
+                    flag--;
+                }
+            }
+            count=1;
+            flag=arr.length;
+        }
+    }
+    public static void output(int arr[][], int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(" "+arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[]args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Input size of matrix:");
+        int n=scanner.nextInt();
+        int arr[][]=new int[n][n];
+        input(arr,n);
+        System.out.println("Your matrix:");
+        output(arr,n);
+    }
+}
