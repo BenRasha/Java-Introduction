1cf9f066c2e29703e71d13a2a27051fd3b03a575
BenRasha <bben.rasha@gmail.com>
2020-03-02 19:21:41 +0300

    Initial commit

---
+++ src/Epam/Algorithms/ArraysOfArrays/Task6.java
@@ -0,0 +1,36 @@
+package Epam.Algorithms.ArraysOfArrays;
+
+import java.util.Scanner;
+public class Task6 {
+    public static void input(int arr[][], int n){
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(((j >= i) && (j < (n - i))) || ((j <= i) && (j > (n - i - 2)))){
+                    arr[i][j]=1;
+                }else {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+    }
+    public static void output(int arr[][], int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(" " + arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input even size of matrix:");
+        int n = scanner.nextInt();
+        int arr[][] = new int[n][n];
+        input(arr, n);
+        System.out.println("Your matrix:");
+        output(arr, n);
+    }
+}
