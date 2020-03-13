bbf366c14110cc8f4d71cd9eb5f3fbe70e036e3d
BenRasha <bben.rasha@gmail.com>
2020-03-13 16:46:20 +0300

    Initial commit

---
+++ src/Epam/Algorithms/Decomposition/Task6.java
@@ -0,0 +1,51 @@
+package Epam.Algorithms.Decomposition;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task6 {
+    public static void fill(int[]array)
+    {
+        Scanner input=new Scanner(System.in);
+        for(int i=0;i<array.length;i++)
+        {
+            array[i]=input.nextInt();
+        }
+    }
+    public static void output(int []array)
+    {
+        System.out.println(Arrays.toString(array));
+    }
+    private static int gcdOfTwo(int a, int b)
+    {
+        while (a-b!=0)
+        {
+            if(a>b){
+                a-=b;
+            }else{
+                b-=a;
+            }
+        }
+        return a;
+    }
+    public static void task(int []array) {
+        int gcd = array[0];
+        for (int i = 0; i < array.length; i++) {
+            gcd = gcdOfTwo(gcd, array[i]);
+        }
+        if(gcd==1){
+            System.out.println("mutually simple");
+        }else{
+            System.out.println("Not mutually simple");
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        int []array=new int[3];
+        fill(array);
+        output(array);
+        task(array);
+    }
+}
