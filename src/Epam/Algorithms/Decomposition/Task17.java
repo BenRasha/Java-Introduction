8b808367d14801dc2ffa143bd25c6c2db9612948
BenRasha <bben.rasha@gmail.com>
2020-07-09 14:26:34 +0300

    Initial commit

---
+++ src/Epam/Algorithms/Decomposition/Task17.java
@@ -0,0 +1,38 @@
+package Epam.Algorithms.Decomposition;
+
+import java.util.ArrayList;
+
+public class Task17 {
+    public static ArrayList<Integer>Number_splitter(int n)
+    {
+        ArrayList<Integer>arrayList=new ArrayList<>();
+        do {
+            arrayList.add(n%10);
+            n/=10;
+        }while(n!=0);
+        return arrayList;
+    }
+    public static int Sum_of_digits(int n)
+    {
+        int result=0;
+        for (int i=0;i<Number_splitter(n).size();i++){
+            result+=Number_splitter(n).get(i);
+        }
+        return result;
+    }
+    public static int task(int n)
+    {
+        int counter=0;
+        do {
+            n-=Sum_of_digits(n);
+            counter++;
+        }while(n!=0);
+        return counter;
+    }
+    public static void main(String[]args)
+    {
+        int n= (int) (0+ Math.random()*100);
+        System.out.println("Input random number: "+n);
+        System.out.println("The number of times to subtract from the number the sum of its digits to get zero:"+task(n));
+    }
+}
