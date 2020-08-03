20f0ad125cfb96bf7cce762b7f0a482a5795dd2e
BenRasha <bben.rasha@gmail.com>
2020-08-03 11:56:13 +0300

    Initial commit

---
+++ src/Epam/StringsAndBasicsOfTextProcessing/WorkingWithAStringAsAnObjectOfTypeString/Task7.java
@@ -0,0 +1,32 @@
+
+public class Task7 {
+    public static String spaceCleaner(String string)
+    {
+        StringBuilder stringBuilder=new StringBuilder();
+        stringBuilder.append(string);
+        for (int i=0;i<stringBuilder.length();i++){
+            if (stringBuilder.charAt(i)==' '){
+                stringBuilder.deleteCharAt(i);
+            }
+        }
+        return stringBuilder.toString();
+    }
+    public static String doubleSymbolCleaner(String string)
+    {
+        StringBuilder stringBuilder=new StringBuilder();
+        stringBuilder.append(spaceCleaner(string));
+        for (int i=0;i<stringBuilder.length();i++){
+            for (int j=i+1;j<stringBuilder.length();j++){
+                if (stringBuilder.charAt(i)==stringBuilder.charAt(j)){
+                    stringBuilder.deleteCharAt(j);
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+    public static void main(String args[])
+    {
+        String text=" abc cde edf";
+        System.out.println(doubleSymbolCleaner(text));
+    }
+}
